d22cf302183923562315cc45a3d2ed05a8eb625e
VitorSales <oslow567@gmail.com>
2024-03-27 15:21:25 -0300

    update

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("test")
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+#Para especificar qual vai ser o navegador que vamos usar
+navegador = webdriver.Chrome()
+#abrindo o site do LINKEDIN
+navegador.get("https://www.linkedin.com/feed/")
+time.sleep(2)
+
