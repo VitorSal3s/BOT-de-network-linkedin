6ba285f528115591cb5a5de5aaf7c98a18805e1f
VitorSales <oslow567@gmail.com>
2024-03-27 17:46:21 -0300

    update

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
+#crucial para acessar a página do usuário as informações de login
+#OBS: Nenhuma das informaçõe fornecdidaas aqui será acessado por terceiros
+def info_login():
+    login = str(input("Você já está logado no site? [s/n]"))
+    if login == "n" or "N":
+        email = str(input("DIGITE SEU EMAIL DE ACESSO: "))
+        senha = str(input("DIGITE SUA SENHA: ")
+    return email, senha
 #Para especificar qual vai ser o navegador que vamos usar
 navegador = webdriver.Chrome()
 #abrindo o site do LINKEDIN
