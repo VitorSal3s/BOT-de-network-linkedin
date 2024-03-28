cec197f84d6c75b7c43ef3d2f86ad1a7efed6225
VitorSales <oslow567@gmail.com>
2024-03-28 19:08:02 -0300

    update

--- main.py
+++ main.py
@@ -9,30 +9,37 @@ import time
 #crucial para acessar a página do usuário as informações de login
 #OBS: Nenhuma das informaçõe fornecdidas aqui será acessado por terceiros
 def info_login():
-    login = str(input("Você já está logado no site? [s/n]"))
-    if login == "n" or "N":
-        email = str(input("DIGITE SEU EMAIL DE ACESSO: "))
-        senha = str(input("DIGITE SUA SENHA: "))
-    else:
-        return none,none
+    print("PRECCISAMOS DE SUAS INFORMAÇÕES DE LOGIN! ")
+    email = str(input("DIGITE SEU EMAIL DE ACESSO: "))
+    senha = str(input("DIGITE SUA SENHA: "))
+
     return email, senha
 
+profissao = input("POR QUAL PROFISSÃO VOCÊ BUSCAR AUMENTAR SUA REDE: ")
 email, senha = info_login()
 #Para especificar qual vai ser o navegador que vamos usar
 navegador = webdriver.Chrome()
+
+navegador.maximize_window()
 #abrindo o site do LINKEDIN
 navegador.get("https://www.linkedin.com/home")
 time.sleep(2)
 
-#O código usa espera explícita para aguardar a presença de um elemento com o ID "session_key" na página. Isso é útil para testes automatizados, garantindo que os elementos necessários estejam disponíveis antes das interações. Ele cria uma instância de WebDriverWait, aguarda até que o elemento esteja presente e, em seguida, permite interações adicionais com o elemento encontrado.
-campo_email = WebDriverWait(navegador, 10).until(
-    EC.presence_of_element_located((By.ID, "session_key"))
-)
-campo_email.send_keys(email)
 
+campo_email = WebDriverWait(navegador, 10).until(EC.presence_of_element_located((By.ID, "session_key")))
+campo_email.send_keys(email)
 campo_senha = navegador.find_element(By.ID, "session_password")
 campo_senha.send_keys(senha)
-pyautogui.press("enter")
-
-
+campo_senha.submit()
 time.sleep(5)
+
+time.sleep(10)
+busca =  WebDriverWait(navegador, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "search-global-typeahead__input")))
+busca.send_keys(profissao)
+pyautogui.press("enter")
+time.sleep(10)
+
+campo_pessoas = navegador.find_element(By.CSS_SELECTOR, '[data-target-section-id="5O3aCjkkT2ehDubv1pCsnQ=="]')
+campo_pessoas.click()
+time.sleep(10)
+
